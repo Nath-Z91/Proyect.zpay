37e734d716a080b6e24fd8c758940b1dfa4b6498
¨NathZ¨ <nathaliagpinilla@gmail.com>
2025-08-09 03:57:58 -0500

    Ej practico #2

---
+++ tienda_ej.py
@@ -0,0 +1,26 @@
+print('*** Sistema de Tienda Online ***')
+
+# Definir las variables de un producto
+nombre_producto = 'Çamara digital' 
+precio_producto = 399.99
+cantidad_inventario = 20
+disponible_entrega = True
+
+# Mostrar informacion del producto
+print('producto:', nombre_producto)
+print('Precio:$', precio_producto)
+print('Çantidad en el cantidad_inventario:', cantidad_inventario)
+print('Disponible para entrega:', disponible_entrega)
+
+#Cambios
+precio_producto = 299.99
+cantidad_inventario = 10
+disponible_entrega = True
+
+# Mostrar informacion del producto
+print()
+print('producto:', nombre_producto)
+print('Precio:$', precio_producto)
+print('Çantidad en el cantidad_inventario:', cantidad_inventario)
+print('Disponible para entrega:', disponible_entrega)
+
