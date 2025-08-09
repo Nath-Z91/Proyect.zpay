2d593c7561b23e08f6a4e268ae67092d871a3ab7
¨NathZ¨ <nathaliagpinilla@gmail.com>
2025-08-09 04:18:38 -0500

    Ej constantes

---
+++ constantes.py
@@ -0,0 +1,14 @@
+import math
+
+
+print('*** Constants en Pyton ***')
+
+PI = 3.14159
+print('El valor de PI es:', PI)
+
+NOMBRE_BASE_DATOS = 'cliente_db'
+print('Nombre de la base de datos:',NOMBRE_BASE_DATOS)
+
+
+#usar constante del lenguaje Python, aunque en este caso no esta en mayuscula
+print('valor de math.pi', math.pi)
